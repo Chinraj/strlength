2c3a591644496f0a540f70c32ac4b74d78360c7e
Chinraj.K <chinrajkalimuthu@gmail.com>
2016-06-30 11:11:45 +0530

    Create strlength.c

---
+++ strlength.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[50];
+int b;
+clrscr();
+printf("Enter the word :\n");
+scanf("%s", a);
+b=strlen(a);
+printf("string length is :%d\n",b);
+getch();
+}
